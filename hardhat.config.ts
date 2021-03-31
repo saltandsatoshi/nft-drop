854a6ea270548af0094921c926d5741fc7fcdfc8
Carl Farterson <carlfarterson@gmail.com>
2021-03-31 15:57:03 -0700

    Add: additional solidity version imports

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,17 +7,19 @@ require("dotenv").config();
 module.exports = {
     solidity: {
         compilers: [
-            {version: "0.6.2"}
+            { version: "0.6.0" },
+            { version: "0.6.2" },
+            { version: "0.8.0" }
         ]
     },
-    networks: {
-        rinkeby: {
-            url: `https://rinkeby.infura.io/v3/${process.env.INFURA_KEY}`,
-            accounts: [`${process.env.PRIVATE_KEY}`,]
-        },
-        mainnet: {
-            url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
-            accounts: [`${process.env.PRIVATE_KEY}`,]
-        }
-    }
+    // networks: {
+    //     rinkeby: {
+    //         url: `https://rinkeby.infura.io/v3/${process.env.INFURA_KEY}`,
+    //         accounts: [`${process.env.PRIVATE_KEY}`,]
+    //     },
+    //     mainnet: {
+    //         url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+    //         accounts: [`${process.env.PRIVATE_KEY}`,]
+    //     }
+    // }
 }
