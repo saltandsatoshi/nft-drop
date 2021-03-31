349ab62bcdc91c66cac0494da4624afdbbb8837a
Carl Farterson <carlfarterson@gmail.com>
2021-03-31 00:28:04 -0700

    Create: TEST_ERC1155.sol

---
+++ contracts/TEST_ERC1155.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
+
+// NOTE: based on https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/mocks/ERC1155Mock.sol
+contract TestERC1155 is ERC1155 {
+    
+    constructor (string memory uri) ERC1155(uri) {
+        // solhint-disable-previous-line no-empty-blocks
+    }
+
+    function setURI(string memory newuri) public {
+        _setURI(newuri);
+    }
+
+    function mint(address to, uint256 id, uint256 value, bytes memory data) public {
+        _mint(to, id, value, data);
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory values, bytes memory data) public {
+        _mintBatch(to, ids, values, data);
+    }
+
+    function burn(address owner, uint256 id, uint256 value) public {
+        _burn(owner, id, value);
+    }
+
+    function burnBatch(address owner, uint256[] memory ids, uint256[] memory values) public {
+        _burnBatch(owner, ids, values);
+    }
+}
--- test/test.ts
+++ test/test.ts
@@ -33,30 +33,23 @@ describe('MerkleDistributor', () => {
     token = await deployContract(wallet0, TestERC20, ['Token', 'TKN', 0], overrides)
   })
 
-  describe('#token', () => {
-    it('returns the token address', async () => {
-      const distributor = await deployContract(wallet0, Distributor, [token.address, ZERO_BYTES32], overrides)
-      expect(await distributor.token()).to.eq(token.address)
-    })
-  })
-
   describe('#merkleRoot', () => {
     it('returns the zero merkle root', async () => {
-      const distributor = await deployContract(wallet0, Distributor, [token.address, ZERO_BYTES32], overrides)
+      const distributor = await deployContract(wallet0, Distributor, ZERO_BYTES32, overrides)
       expect(await distributor.merkleRoot()).to.eq(ZERO_BYTES32)
     })
   })
 
   describe('#claim', () => {
     it('fails for empty proof', async () => {
-      const distributor = await deployContract(wallet0, Distributor, [token.address, ZERO_BYTES32], overrides)
+      const distributor = await deployContract(wallet0, Distributor, ZERO_BYTES32, overrides)
       await expect(distributor.claim(0, wallet0.address, 10, [])).to.be.revertedWith(
         'MerkleDistributor: Invalid proof.'
       )
     })
 
     it('fails for invalid index', async () => {
-      const distributor = await deployContract(wallet0, Distributor, [token.address, ZERO_BYTES32], overrides)
+      const distributor = await deployContract(wallet0, Distributor, ZERO_BYTES32, overrides)
       await expect(distributor.claim(0, wallet0.address, 10, [])).to.be.revertedWith(
         'MerkleDistributor: Invalid proof.'
       )
@@ -70,7 +63,7 @@ describe('MerkleDistributor', () => {
           { account: wallet0.address, amount: BigNumber.from(100) },
           { account: wallet1.address, amount: BigNumber.from(101) },
         ])
-        distributor = await deployContract(wallet0, Distributor, [token.address, tree.getHexRoot()], overrides)
+        distributor = await deployContract(wallet0, Distributor, treegetHexRoot()], overrides)
         await token.setBalance(distributor.address, 201)
       })
 
@@ -182,7 +175,7 @@ describe('MerkleDistributor', () => {
             return { account: wallet.address, amount: BigNumber.from(ix + 1) }
           })
         )
-        distributor = await deployContract(wallet0, Distributor, [token.address, tree.getHexRoot()], overrides)
+        distributor = await deployContract(wallet0, Distributor, treegetHexRoot()], overrides)
         await token.setBalance(distributor.address, 201)
       })
 
@@ -225,7 +218,7 @@ describe('MerkleDistributor', () => {
       })
 
       beforeEach('deploy', async () => {
-        distributor = await deployContract(wallet0, Distributor, [token.address, tree.getHexRoot()], overrides)
+        distributor = await deployContract(wallet0, Distributor, treegetHexRoot()], overrides)
         await token.setBalance(distributor.address, constants.MaxUint256)
       })
       it('no double claims in random distribution', async () => {
@@ -257,7 +250,7 @@ describe('MerkleDistributor', () => {
       })
       expect(tokenTotal).to.eq('0x02ee') // 750
       claims = innerClaims
-      distributor = await deployContract(wallet0, Distributor, [token.address, merkleRoot], overrides)
+      distributor = await deployContract(wallet0, Distributor, merkleRoot, overrides)
       await token.setBalance(distributor.address, tokenTotal)
     })
 
