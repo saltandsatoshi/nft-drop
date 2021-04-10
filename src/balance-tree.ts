4d69859ffe84609ca53338719649cbbffb862d87
Carl Farterson <carlfarterson@gmail.com>
2021-04-10 15:12:42 -0700

    Refactor: BigNumber to number

--- src/balance-tree.ts
+++ src/balance-tree.ts
@@ -3,7 +3,7 @@ import { BigNumber, utils } from 'ethers'
 
 export default class BalanceTree {
   private readonly tree: MerkleTree
-  constructor(balances: { account: string; amount: BigNumber }[]) {
+  constructor(balances: { account: string; amount: number }[]) {
     this.tree = new MerkleTree(
       balances.map(({ account, amount }, index) => {
         return BalanceTree.toNode(index, account, amount)
@@ -12,9 +12,9 @@ export default class BalanceTree {
   }
 
   public static verifyProof(
-    index: number | BigNumber,
+    index: number,
     account: string,
-    amount: BigNumber,
+    amount: number,
     proof: Buffer[],
     root: Buffer
   ): boolean {
@@ -27,7 +27,7 @@ export default class BalanceTree {
   }
 
   // keccak256(abi.encode(index, account, amount))
-  public static toNode(index: number | BigNumber, account: string, amount: BigNumber): Buffer {
+  public static toNode(index: number, account: string, amount: number): Buffer {
     return Buffer.from(
       utils.solidityKeccak256(['uint256', 'address', 'uint256'], [index, account, amount]).substr(2),
       'hex'
@@ -39,7 +39,7 @@ export default class BalanceTree {
   }
 
   // returns the hex bytes32 values of the proof
-  public getProof(index: number | BigNumber, account: string, amount: BigNumber): string[] {
+  public getProof(index: number, account: string, amount: number): string[] {
     return this.tree.getHexProof(BalanceTree.toNode(index, account, amount))
   }
 }
